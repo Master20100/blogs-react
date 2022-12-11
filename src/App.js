30fba0cce9b729f2f84cb0e6a028dd23938b0e02
Mina Ghaly <minanabil@msn.com>
2022-12-11 18:42:36 +1100

    nav bar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
+  const title = 'Welcome to the new Blog';
+  const link = 'http://www.google.com';
   return (
     <div className="App">
-      
+      <Navbar />
+      <h1>{title}</h1>
+      <a href={link}>Google</a>
       
     </div>
   );
