37536110307450f63e6e45a87c173a20f477f6d8
Mina Ghaly <minanabil@msn.com>
2022-12-14 21:28:51 +1100

    simple blog structure added

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,25 @@
+import {useState} from 'react';
 const Home = () => {
+const [blogs,setBlogs]  = useState([
+    {title:"blog 1",body:"content1",author:"author1",id:1},
+    {title:"blog 2",body:"content2",author:"author2",id:2},
+    {title:"blog 3",body:"content3",author:"author3",id:3}
+]);    
+
     return ( 
         <div className="home">
-        <h2>Home Page</h2>
+        {blogs.map((blog)=>(
+<div className="blog-preview" key={blog.id}> 
+<h2>{blog.body}</h2>
+<p>Written by {blog.author}</p>
+
+
+</div>
+
+))}
         </div>
      );
 }
- 
 export default Home;
+
+
