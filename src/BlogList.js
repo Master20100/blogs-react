cdfef41dbdfd1e6fbff46b9e81eda96b743e6737
Mina Ghaly <minanabil@msn.com>
2022-12-18 19:01:32 +1100

    blog delete button added

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,10 +1,12 @@
-const BlogList = ({blogs}) => {
+const BlogList = ({blogs,handleDelete}) => {
+        
 return(
     <div className="blog-list">
     {blogs.map((blog)=>(
     <div className="blog-preview" key={blog.id}> 
       <h2>{blog.body}</h2>
       <p>Written by {blog.author}</p>
+      <button onClick={()=>handleDelete(blog.id)}>delete blog</button>
     </div>
         ))}
     </div>
--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,22 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 const Home = () => {
 const [blogs,setBlogs]  = useState([
     {title:"blog 1",body:"content1",author:"author1",id:1},
     {title:"blog 2",body:"content2",author:"author2",id:2},
-    {title:"blog 3",body:"content3",author:"author3",id:3}
+    {title:"blog 2",body:"content3",author:"author3",id:3}
 ]);    
 
-    return ( 
-<div className='home'>
-    <BlogList blogs={blogs} />
-
-</div>
-);
+const handleDelete = (id)=>{
+    const newBlogs = blogs.filter(blog=>blog.id!==id);
+    setBlogs(newBlogs);
+}
+    
+return ( 
+  <div className='home'>
+    <BlogList blogs={blogs} handleDelete ={handleDelete}/>
+  </div>
+ );
 }
 export default Home;
 
