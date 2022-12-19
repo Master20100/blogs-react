ff9f08bbee323e91656f3ae22b8971178dba55b1
Mina Ghaly <minanabil@msn.com>
2022-12-19 20:51:40 +1100

    simple blogs data added to the json server

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,7 @@
 import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 const Home = () => {
-const [blogs,setBlogs]  = useState([
-    {title:"blog 1",body:"content1",author:"author1",id:1},
-    {title:"blog 2",body:"content2",author:"author2",id:2},
-    {title:"blog 2",body:"content3",author:"author3",id:3}
-]);    
+const [blogs,setBlogs]  = useState(null);    
 
 const handleDelete = (id)=>{
     const newBlogs = blogs.filter(blog=>blog.id!==id);
