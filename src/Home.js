5773fe99db480ca3effe6b35f616e41986391963
Mina Ghaly <minanabil@msn.com>
2022-12-20 21:19:13 +1100

    loading paragraph added to show to user before rendering is finished

--- src/Home.js
+++ src/Home.js
@@ -2,16 +2,30 @@ import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 const Home = () => {
 const [blogs,setBlogs]  = useState(null);    
+const [isPending,setIsPending]  = useState(true);    
 
 const handleDelete = (id)=>{
     const newBlogs = blogs.filter(blog=>blog.id!==id);
     setBlogs(newBlogs);
 }
     
-useEffect(()=>{console.log("use effect")},[blogs]);
+useEffect(()=>{
+    fetch("http://localhost:8000/blogs")
+    .then(res=>{
+        return res.json();
+    })
+    .then(data =>{
+        setBlogs(data);
+        setIsPending(false);
+    })
+    
+},[]);
+
+
 return ( 
   <div className='home'>
-    <BlogList blogs={blogs} handleDelete ={handleDelete}/>
+    {isPending && <p>...loading</p>}
+    {blogs && <BlogList blogs={blogs} handleDelete ={handleDelete}/>}
   </div>
  );
 }
